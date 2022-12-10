e89b1b25de4e1d93d266f9713b41a05a0d4116ad
Tornelius <tkbjr28@icloud.com>
2022-12-10 11:58:32 -0500

    Initial Commit

---
+++ MovieOS/MovieOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieOSApp.swift
+//  MovieOS
+//
+//  Created by Tornelius Broadwater, Jr on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
